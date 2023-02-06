b108cf3dbf0a89f65a057659c8ffb0523694babf
Nguyễn Tiến Thành <tienthanh.jlw@gmail.com>
2023-02-06 11:29:26 +0700

    Exercise 1

---
+++ src/Exercise1/Exercise1.java
@@ -0,0 +1,25 @@
+package Exercise1;
+
+public class Exercise1 {
+    public static void part1() {
+        /*
+        * Manipulate math operations with "a" and "b"
+        * */
+        System.out.println("Exercise 1. part 1");
+        int a = 10;
+        int b = 5;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a -b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (double) (a/b));
+
+        /*
+        * Comparison between ++i and i++
+        * */
+        int i = 5;
+        System.out.println("i =" + i);
+        System.out.println("i++ =" + i++);
+        System.out.println("++i =" + ++i);
+        System.out.println("i = " + i);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import Exercise1.Exercise1;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Exercise1.part1();
     }
 }
